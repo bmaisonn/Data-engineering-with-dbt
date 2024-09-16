c74abfc514380a8ac0a1b728e6f69a7e697ed04f
Bertrand Maisonneuve <bmaisonn@gmail.com>
2024-09-16 12:21:07 +0000

    wip

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,9 +1,14 @@
-WITH 
-current_snapshot_from as (
-    {{ current_from_snapshot(snsh_ref = ref('SNSH_ABC_BANK_POSITION')) }}
+WITH
+
+current_from_history as (
+    {{ current_from_history(
+        history_rel = ref('HIST_ABC_BANK_POSITION'), 
+        key_column = 'POSITION_HKEY',
+    ) }}
 )
-SELECT
+
+SELECT 
     *
     , POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT
-    , ROUND(UNREALIZED_PROFIT / COST_BASE, 5) as UNREALIZED_PROFIT_PCT
-FROM current_snapshot_from
+    , ROUND(UNREALIZED_PROFIT / COST_BASE, 5)*100 as UNREALIZED_PROFIT_PCT
+FROM current_from_history
